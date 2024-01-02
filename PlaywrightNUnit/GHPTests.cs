24132ab8021d205cfdc7c222424bdc6bd2e40020
ParvathyPanayingal <ParvathyPanayingal@ust.com>
2024-01-02 15:42:04 +0530

    daily work 2-1-2024

--- PlaywrightNUnit/GHPTests.cs
+++ PlaywrightNUnit/GHPTests.cs
@@ -1,44 +1,71 @@
 using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
 
 namespace PlaywrightNUnit
 {
-    public class GHPTests
+    public class GHPTests :PageTest
     {
         [SetUp]
         public void Setup()
         {
         }
+       
+        //[Test]
+        //public async Task Test1()
+        //{
+        //    using var playwright = await Playwright.CreateAsync();
 
+        //    //launch browser
+        //    await using var browser = await playwright.Chromium.LaunchAsync();
+
+        //    //page instance
+        //    var context = await browser.NewContextAsync();
+        //    var page = await context.NewPageAsync();
+
+        //    Console.WriteLine("Opened Browser");
+        //    await page.GotoAsync("https://www.google.com");
+        //    Console.WriteLine("Page Loaded");
+
+        //    string title = await page.TitleAsync();
+        //    Console.WriteLine(title);
+
+        //    await page.GetByTitle("Search").FillAsync("hp laptop");
+        //    //await page.Locator("#APjFgb").FillAsync("hp laptop");//another method,# for id,. for class
+        //    Console.WriteLine("Typed");
+        //    await page.Locator("(//input[@value='Google Search'])[2]").ClickAsync();
+        //    Console.WriteLine("Clicked");
+
+        //    title = await page.TitleAsync();
+        //    Console.WriteLine(title);
+
+
+
+        //}
+
+        //playwright managed instance
         [Test]
-        public async Task Test1()
+        public async Task Test2()
         {
-            using var playwright = await Playwright.CreateAsync();
-
-            //launch browser
-            await using var browser = await playwright.Chromium.LaunchAsync();
-
-            //page instance
-            var context = await browser.NewContextAsync();
-            var page = await context.NewPageAsync();
 
             Console.WriteLine("Opened Browser");
-            await page.GotoAsync("https://www.google.com");
+            await Page.GotoAsync("https://www.google.com");
             Console.WriteLine("Page Loaded");
 
-            string title = await page.TitleAsync();
+            string title = await Page.TitleAsync();
             Console.WriteLine(title);
 
-            await page.GetByTitle("Search").FillAsync("hp laptop");
+            await Page.GetByTitle("Search").FillAsync("hp laptop");
             //await page.Locator("#APjFgb").FillAsync("hp laptop");//another method,# for id,. for class
             Console.WriteLine("Typed");
-            await page.Locator("(//input[@value='Google Search'])[2]").ClickAsync();
+            await Page.Locator("(//input[@value='Google Search'])[2]").ClickAsync();
             Console.WriteLine("Clicked");
 
-            title = await page.TitleAsync();
+            title = await Page.TitleAsync();
             Console.WriteLine(title);
 
 
-
+            Assert.That(title, Does.Contain("hp laptop"));
+            //Expect(Page.TitleAsync()).Equals(""hp laptop"));
         }
     }
 }
