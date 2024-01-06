39bab86388d8164c082cb29334e8516522da0371
ParvathyPanayingal <ParvathyPanayingal@ust.com>
2024-01-06 15:25:51 +0530

    Casestudy 4-1-2024

---
+++ AmazonPW/Test Helper Classes/AmazonText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonPW.Test_Data_Classes
+{
+    public class AmazonText
+    {
+        public string? SearchText { get; set; }
+
+    }
+}
---
+++ AmazonPW/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AmazonPW
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
