b7af45ffe2648b970e57da233d58580b97b0e75d
ParvathyPanayingal <ParvathyPanayingal@ust.com>
2024-01-06 15:33:30 +0530

    Assignment 5-1-2024

---
+++ JSONPlaceholderAPI/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace JSONPlaceholderAPI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
